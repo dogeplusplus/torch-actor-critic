fa30c1ab8bb38cda1c655ab51b3f52d50b815b8a
dogeplusplus <albertswchung@gmail.com>
2022-02-19 12:19:09 +0000

    Change from using lazylinear to linear. Implement visual critic and double critic, add simple shape test.

--- networks/convolutional.py
+++ networks/convolutional.py
@@ -4,10 +4,27 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
+from torch import FloatTensor
 from torch.distributions.normal import Normal
 
 from networks.core import mlp
 from environments.wall_runner import MultiObservation
+
+
+def calculate_size(
+    image_shape: t.Tuple[int, int, int],
+    filters: t.List[int],
+    kernel_sizes: t.List[int],
+    strides: t.List[int],
+) -> int:
+    image_shape = list(image_shape)
+
+    for f, k, s in zip(filters, kernel_sizes, strides):
+        image_shape[0] = f
+        image_shape[1] = int(np.floor((image_shape[1] - k) / s + 1))
+        image_shape[2] = int(np.floor((image_shape[2] - k) / s + 1))
+
+    return np.prod(image_shape)
 
 
 def simple_cnn(
@@ -26,14 +43,15 @@ def simple_cnn(
         model.add_module(f"conv_{i}", nn.Conv2d(sizes[i], sizes[i+1], kernel_sizes[i], strides[i]))
         model.add_module(f"relu_{i}", activation())
 
+    conv_shape = calculate_size(input_shape, filters, kernel_sizes, strides)
     model.add_module("flatten", nn.Flatten())
-    model.add_module("linear", nn.LazyLinear(dense_size))
+    model.add_module("linear", nn.Linear(conv_shape, dense_size))
     model.add_module("final", nn.Linear(dense_size, 1))
 
     return model
 
 
-class MultiModalActor(nn.Module):
+class VisualActor(nn.Module):
     def __init__(
         self,
         obs_dim: int,
@@ -47,7 +65,7 @@ class MultiModalActor(nn.Module):
         kernel_sizes: t.List[int] = [8, 4, 3],
         strides: t.List[int] = [4, 2, 1],
     ):
-        super(MultiModalActor, self).__init__()
+        super(VisualActor, self).__init__()
         self.layers = mlp([obs_dim] + hidden_sizes)
 
         self.obs_dim = obs_dim
@@ -68,7 +86,7 @@ class MultiModalActor(nn.Module):
         x: MultiObservation,
         deterministic: bool = False,
         with_logprob: bool = True
-    ):
+    ) -> FloatTensor:
         image = x.camera
         image = image.view((-1,) + self.vis_dim)
         x = x.features
@@ -98,3 +116,61 @@ class MultiModalActor(nn.Module):
             logprob -= (2 * torch.as_tensor(np.log(2)) - prob - F.softplus(-2*prob)).sum(dim=-1)
 
         return (torch.squeeze(pi_action), torch.squeeze(logprob))
+
+
+class VisualCritic(nn.Module):
+    def __init__(
+        self,
+        obs_dim: int,
+        act_dim: int,
+        vis_dim: t.Tuple[int, int, int],
+        hidden_sizes: t.List[int] = [256, 256],
+        filters: t.List[int] = [32, 64, 64],
+        kernel_sizes: t.List[int] = [8, 4, 3],
+        strides: t.List[int] = [4, 2, 1],
+    ):
+        super(VisualCritic, self).__init__()
+        self.obs_dim = obs_dim
+        self.act_dim = act_dim
+        self.vis_dim = vis_dim
+
+        # Extra neuron in penultimate fully connected layer for visual information
+        self.layers = mlp([obs_dim + act_dim] + hidden_sizes + [1])
+        self.final = nn.Linear(2, 1)
+        self.visual_network = simple_cnn(vis_dim, filters, kernel_sizes, strides)
+
+    def forward(self, x: MultiObservation) -> FloatTensor:
+        image = x.camera
+        image = image.view((-1,) + self.vis_dim)
+        conv_output = self.visual_network(image)
+
+        x = x.features
+        x = x.view((-1, self.obs_dim + self.act_dim))
+        for layer in self.layers:
+            x = layer(x)
+            x = F.relu(x)
+
+        x = torch.cat([x, conv_output], dim=1)
+        x = self.final(x)
+        x = torch.squeeze(x, -1)
+
+        return x
+
+
+class VisualDoubleCritic(nn.Module):
+    def __init__(
+        self,
+        obs_dim: int,
+        act_dim: int,
+        vis_dim: t.Tuple[int, int, int],
+        hidden_sizes: t.List[int] = [256, 256],
+        filters: t.List[int] = [32, 64, 64],
+        kernel_sizes: t.List[int] = [8, 4, 3],
+        strides: t.List[int] = [4, 2, 1],
+    ):
+        super(VisualDoubleCritic, self).__init__()
+        self.q1 = VisualCritic(obs_dim, act_dim, vis_dim, hidden_sizes, filters, kernel_sizes, strides)
+        self.q2 = VisualCritic(obs_dim, act_dim, vis_dim, hidden_sizes, filters, kernel_sizes, strides)
+
+    def forward(self, x: MultiObservation) -> t.Tuple[FloatTensor, FloatTensor]:
+        return self.q1(x), self.q2(x)
--- tests/test_convolutional.py
+++ tests/test_convolutional.py
@@ -1,14 +1,14 @@
 import torch
 
-from networks.convolutional import MultiModalActor
+from networks.convolutional import VisualActor, VisualCritic
 from environments.wall_runner import MultiObservation
 
 
-def test_multimodal():
+def test_visual_actor():
     obs_dim = 20
     act_dim = 10
     vis_dim = (3, 64, 64)
-    model = MultiModalActor(obs_dim, act_dim, vis_dim)
+    model = VisualActor(obs_dim, act_dim, vis_dim)
 
     features = torch.rand((obs_dim,))
     camera = torch.rand(vis_dim)
@@ -17,3 +17,22 @@ def test_multimodal():
 
     assert list(pi.size()) == [act_dim]
     assert list(log_prob.size()) == []
+
+
+def test_visual_double_critics():
+    obs_dim = 20
+    act_dim = 10
+    vis_dim = (3, 64, 64)
+    model = VisualCritic(obs_dim, act_dim, vis_dim)
+
+    state_action = torch.rand((2, obs_dim + act_dim))
+    camera = torch.rand((2,) + vis_dim)
+    x = MultiObservation(state_action, camera)
+    value = model(x)
+    assert list(value.size()) == [2]
+
+    state_action = torch.rand((obs_dim + act_dim,))
+    camera = torch.rand(vis_dim)
+    x = MultiObservation(state_action, camera)
+    value = model(x)
+    assert list(value.size()) == [1]
